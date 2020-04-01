fc4d1f8e7ddc9609199ec8e21ba429cebb010d0e
robinrastogi <robinrastogigcet@gmail.com>
2020-04-02 00:30:21 +0530

    updated with logic

--- CoreJava/sample_Project/src/exceptionalHandling/TryCatchFinally.java
+++ CoreJava/sample_Project/src/exceptionalHandling/TryCatchFinally.java
@@ -1,5 +1,16 @@
 package exceptionalHandling;
 
+/**
+ * @author Robin
+ * 1) If finally block returns a value then try and catch blocks may or may not return a value.
+ * 2) If finally block does not return a value then both try and catch blocks must return a value.
+ * 3) If try-catch-finally blocks are returning a value according to above rules, 
+ * 		then you should not keep any statements after finally block. 
+ * 		Because they become unreachable and in Java, Unreachable code gives compile time error.
+ * 4) finally block overrides any return values from try and catch blocks.
+ * 5) finally block will be always executed even though try and catch blocks are returning the control.
+ * 
+ */
 public class TryCatchFinally {
 
 	public static void main(String[] args) {
