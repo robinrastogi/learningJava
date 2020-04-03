babd8a6d05e0279cee8385a491924295630f40bc
robinrastogi <robinrastogigcet@gmail.com>
2020-04-04 00:02:35 +0530

    initial upload

---
+++ Spring/spring-demo-annotations/src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,33 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
+		
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+				
+		// call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println("email: " + theCoach.getEmail());
+		
+		System.out.println("team: " + theCoach.getTeam());
+					
+		// close the context
+		context.close();
+		
+	}
+
+}
+
+
---
+++ Spring/spring-mvc-demo2/WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+	<h2>Spring MVC Demo - Home Page</h2>
+
+	<hr>
+
+	<a href="hello/showForm">Hello World form</a>
+
+	<a href="student/showForm">Student form</a>
+
+	<a href="customer/showForm">Customer form</a>
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
