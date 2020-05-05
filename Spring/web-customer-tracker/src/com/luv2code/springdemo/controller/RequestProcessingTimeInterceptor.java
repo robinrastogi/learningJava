548502289a86f48cf2c67595fa9af4b9e5c3c487
robinrastogi <robinrastogigcet@gmail.com>
2020-05-05 20:47:17 +0530

    added interceptor handler class

---
+++ Spring/web-customer-tracker/src/com/luv2code/springdemo/controller/RequestProcessingTimeInterceptor.java
@@ -0,0 +1,48 @@
+package com.luv2code.springdemo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RequestProcessingTimeInterceptor extends HandlerInterceptorAdapter {
+
+	/*public RequestProcessingTimeInterceptor(WebRequestInterceptor requestInterceptor) {
+		super(requestInterceptor);
+		// TODO Auto-generated constructor stub
+	}*/
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		long startTime = System.currentTimeMillis();
+		System.out.println("Request URL::" + request.getRequestURL().toString()
+				+ ":: Start Time=" + System.currentTimeMillis());
+		request.setAttribute("startTime", startTime);
+		//if returned false, we need to make sure 'response' is sent
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("Request URL::" + request.getRequestURL().toString()
+				+ " Sent to Handler :: Current Time=" + System.currentTimeMillis());
+		//we can add attributes in the modelAndView and use that in the view page
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long startTime = (Long) request.getAttribute("startTime");
+		System.out.println("Request URL::" + request.getRequestURL().toString()
+				+ ":: End Time=" + System.currentTimeMillis());
+		System.out.println("Request URL::" + request.getRequestURL().toString()
+				+ ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+	}
+
+}
+
