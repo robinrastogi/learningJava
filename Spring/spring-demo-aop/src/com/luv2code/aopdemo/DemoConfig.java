5dc6b708613aadaf5047a74be2e703551697a1c0
robinrastogi <robinrastogigcet@gmail.com>
2020-06-09 19:45:44 +0530

    added java doc

--- Spring/spring-demo-aop/src/com/luv2code/aopdemo/DemoConfig.java
+++ Spring/spring-demo-aop/src/com/luv2code/aopdemo/DemoConfig.java
@@ -9,4 +9,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan("com.luv2code.aopdemo")
 public class DemoConfig {
 
+	//the annotations used in this class are similar to the below xml configurations
+	/*
+	<aop:aspectj-autoproxy />
+	 
+	<context:component-scan base-package="com.luv2code.aopdemo" />
+	 
+	<bean id="loggingAspect" class="com.luv2code.aopdemo.DemoConfig" />
+	*/
 }
